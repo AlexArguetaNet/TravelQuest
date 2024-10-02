7f43bfdc0c1871174ad2706ce7f847e90bfcf2f2
Alex Argueta <alexarguetanet@gmail.com>
2024-10-02 15:28:44 -0400

    Completed Tailwind css setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
 
 const App = () => {
   return <>
-    <div>App</div>
+    <div>
+      <Router>
+
+        <Routes>
+          <Route path="/dashboard" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/signup" element={<SignUp />}/>
+          
+        </Routes>
+      </Router>
+    </div>
   </>
 }
 
